e82388973feb5b1b8e7ecface0ad7d1e37db1ec3
LiamEBirge <liamebirge@gmail.com>
2018-02-04 15:45:56 -0800

    Adding random image

--- JS/script.js
+++ JS/script.js
@@ -7,3 +7,8 @@ function gotoNew(url){
   var win = window.open(url, '_blank');
   win.focus();
 }
+
+function chooseImg(){
+	var randNum = Math.floor((Math.random() * 106) + 1);
+	document.getElementById("indexPortfolioImage").src = "Data/Photos/img (" + randNum + ").JPG";
+}
